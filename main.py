3832ee91b7ca9fdb11631a999b9788da76e19d44
ZinkovskiyRuslan <zinkovsky.ruslan@yandex.ru>
2021-06-21 07:06:37 +0100

    add ultrasonic distance sensor

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from telegram_bot import send_telegram
 from temperature import body_sensor
 from bluetooth_adaptor_info import bluetooth_adaptor_info
+from ultrasonic_distance_sensor import distance
 
-send_telegram("Start programm:  \n" + bluetooth_adaptor_info() + "\nTemperature: " + body_sensor() + "°C")
+send_telegram("Start programm:  \n" + bluetooth_adaptor_info() + "\nTemperature: " + body_sensor() + "°C\nDistance: " + str(distance()) + "cm")
 
